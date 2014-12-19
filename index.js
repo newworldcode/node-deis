577afb6ab4949073a498397495385a3c9026b866
davemackintosh <me@davemackintosh.co.uk>
2014-12-19 15:41:00 +0000

    Login now working.

---
+++ index.js
@@ -0,0 +1,87 @@
+/**
+ * Prepare the interface.
+ *
+ * @author Dave Mackintosh
+ * @version 1.0.0
+ * @since   1.0.0
+ */
+
+
+function NodeDeis(configuration) {
+  'use strict';
+
+  // Always create an instance.
+  if (!(this instanceof NodeDeis)) {
+    return new NodeDeis(configuration);
+  }
+
+  // The auth token is private.
+  var _token = null;
+
+  // Check for configuration.
+  if (!configuration) {
+    throw new ReferenceError('Node Deis requires configuration to work.');
+  }
+
+  // Check we got required config properties.
+  if (!configuration.hasOwnProperty('controller')) {
+    throw new ReferenceError('Node Deis configuration requires controller property.');
+  }
+
+  if (!configuration.hasOwnProperty('username')) {
+    throw new ReferenceError('Node Deis configuration requires username property.');
+  }
+
+  if (!configuration.hasOwnProperty('password')) {
+    throw new ReferenceError('Node Deis configuration requires password property.');
+  }
+
+  // If we explicitely set it to a secure then we'll https it.
+  this.protocol = configuration.secure ? 'https' : 'http';
+
+  // Set the controller, we haven't errored if we got here.
+  this.controller = configuration.controller;
+
+  // Support future (and previous) versions.
+  this.version = configuration.version ? 'v' + configuration.version : 'v1';
+
+  // We don't ever want to set the username or password to anything
+  // set up getters to get the values from the config argument.
+  this.__defineGetter__('username', function() {
+    return configuration.username;
+  });
+
+  this.__defineGetter__('password', function() {
+    return configuration.password;
+  })
+
+  // Do some checking on the virtual token property
+  // to make sure we're secured.
+  this.__defineGetter__('token', function() {
+    if (_token) {
+      return _token;
+    } else {
+      return null;
+    }
+  });
+
+  this.__defineSetter__('token', function(new_value) {
+    if (new_value && new_value !== null) {
+      _token = new_value;
+    } else {
+      throw new TypeError('Cannot set token to null.');
+    }
+  });
+
+}
+
+// Add the extra methods.
+// If we log in correctly, more methods will be added.
+NodeDeis.prototype = {
+  connect: require('./lib/connect'),
+  addDomain: require('./lib/domain').add
+  removeDomain: require('./lib/domain').remove
+};
+
+// Export.
+module.exports = NodeDeis;
